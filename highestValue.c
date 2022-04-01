2194d5cf12fe2065fd0d8ea43a8e0eade4520f11
alamin5g <alaminrakib4864@gmail.com>
2022-04-01 19:45:55 +0600

    find the highest value of its input.

--- highestValue.c
+++ highestValue.c
@@ -1,44 +1,45 @@
 /**
  * @file highestValue.c
- * 33. Write a C program that accepts some integers from the user and find the highest value and the input position. Go to the editor
-Test Data :
-Input 5 integers:
-5
-7
-15
-23
-45
-Expected Output:
-Highest value: 45
-Position: 5
+ * find the highest value from an array.
  * @author Md. Alamin (alamin5g@yahoo.com)
  * I would love be a software engineer at Google. That is why anybody can uses this code without any condition, if you face any difficulty, then try to email me.
  * @version 0.1
- * @date 2022-03-28
- *
+ * @date 2022-04-01
+ * 
  * @copyright Copyright (c) 2022
- *
+ * 
  */
+
+
 #include <stdio.h>
 
 int main()
 {
-    int arraySize, i, position = 0, maxValue = 0;
+    int n, t = 0, pos, i;
     printf("Enter array length: \n");
-    scanf("%d", &arraySize);
-    int a[arraySize];
-    for (i = 1; i <= arraySize; i++)
+    scanf("%d", &n);
+    int a[n];
+    printf("Enter array value: \n");
+    for (i = 0; i < n; i++)
     {
         scanf("%d", &a[i]);
-
-        if (a[i] > maxValue)
-        {
-            maxValue = a[i];
-            position = i;
-        }
     }
 
-    printf("Highest Value: %d\n", maxValue);
-    printf("Position: %d\n", position);
+    t = a[0];
+    pos = 0;
+    
+    for (i = 1; i < n; i++)
+    {
+        if (t < a[i])
+        {
+            t = a[i];
+            pos = i+1;
+        }
+        
+      
+    }
+    printf("Highest value is : %d\n", t);
+    printf("Highest value position is : %d\n", pos);
+
     return 0;
 }
