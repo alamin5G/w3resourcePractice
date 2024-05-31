904070f46b29597ca819a693fc84ef20b9e7d651
alamin5g <21303134@iubat.edu>
2024-06-01 03:36:24 +0600

    Current Date Time of System with specific format

--- java_basic_1/CurrentDateTime.java
+++ java_basic_1/CurrentDateTime.java
@@ -1,6 +1,9 @@
 package java_basic_1;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.TimeZone;
 
 public class CurrentDateTime {
     public static void main(String[] args) {
@@ -9,5 +12,9 @@ public class CurrentDateTime {
         Date d = new Date();
         d.setTime(System.currentTimeMillis());
         System.out.format("Current Date time: %tc%n", d.getTime());
+
+        SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
+        sdf.setCalendar(Calendar.getInstance(TimeZone.getTimeZone("GMT+5")));
+        System.out.println("Current Date time: " + sdf.format(d));
     }
 }
