fe85576f3a4a49fce9d525126d7efaa853914ceb
alamin5g <21303134@iubat.edu>
2024-06-23 09:01:33 +0600

    Triangle shape incomplete

--- ConditionalStatement/RightTriangle.java
+++ ConditionalStatement/RightTriangle.java
@@ -29,7 +29,7 @@ public class RightTriangle {
         int c = 1;
         for (int i = 1; i < 5; i++) {
             for (int j =1; j <=i; j++) {
-                System.out.print(c);
+                System.out.print(c+ " ");
                 c++;
             }
             System.out.println();
