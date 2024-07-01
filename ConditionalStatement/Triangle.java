dcb0fe6b8f93bed489f0252b9319aae85534ff60
alamin5g <alaminrakib4864@gmail.com>
2024-07-01 12:08:57 +0600

    triangle 1 is done

--- ConditionalStatement/Triangle.java
+++ ConditionalStatement/Triangle.java
@@ -1,15 +1,27 @@
 package ConditionalStatement;
 
+import java.util.Scanner;
+
 public class Triangle {
 
     public static void main(String[] args) {
 
+        System.out.println("Enter the triangle length");
+        Scanner sc = new Scanner(System.in);
+        double a = sc.nextDouble();
+
         System.out.println("Triangle 1: ");
 
-        for (int i = 1; i <=4 ; i++) {
-            for (int j = 1; j <=4 ; j++) {
-
+        for (int i = 1; i <=a ; i++) {
+            //print the front spaces
+            for (int j = i; j <a ; j++) {
+                System.out.print(" ");
             }
+            //print the number
+            for (int j = 1; j <=i ; j++) {
+                System.out.print("" + i + " ");
+            }
+            System.out.println();
         }
     }
 }
